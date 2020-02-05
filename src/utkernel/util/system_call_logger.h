0ba1aead1f4c192ab3b7ec754217e8c952b849c0
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-02-05 22:41:01 +0900

    Add systemCallLogger module

---
+++ src/utkernel/util/system_call_logger.h
@@ -0,0 +1,20 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef SRC_UTKERNEL_UTIL_SYSTEM_CALL_LOGGER_H_
+#define SRC_UTKERNEL_UTIL_SYSTEM_CALL_LOGGER_H_
+
+enum {
+  kNonsenseParameter = 0xdeadbeef,
+};
+
+typedef struct {
+  void (*Reset)(void);
+  void (*Enter)(const char* system_call_name, int id);
+  void (*Exit)(const char* system_call_name, int return_code);
+  const char* (*Get)(void);
+} SystemCallLoggerMethodStruct;
+typedef const SystemCallLoggerMethodStruct* SystemCallLoggerMethod;
+
+extern const SystemCallLoggerMethod systemCallLogger;
+
+#endif  // SRC_UTKERNEL_UTIL_SYSTEM_CALL_LOGGER_H_
