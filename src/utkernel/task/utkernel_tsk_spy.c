941619cf33a1eae9748a948a28e1dfb7d2e71f9e
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-02-08 12:15:41 +0900

    Use systemCallTemplate instead of systemCallLogger

--- src/utkernel/task/utkernel_tsk_spy.c
+++ src/utkernel/task/utkernel_tsk_spy.c
@@ -4,7 +4,7 @@
 
 #include <string.h>
 
-#include "../util/system_call_logger.h"
+#include "../util/system_call_template.h"
 
 static void *its_exinf;
 static ATR its_attribute;
@@ -17,7 +17,7 @@ static RELTIM its_delay_time;
 static int number_of_executions;
 static INT return_codes[8];
 
-inline static void ResetTaskInfo(void) {
+static void Reset(void) {
   its_exinf = NULL;
   its_attribute = 0;
   its_task = NULL;
@@ -25,9 +25,6 @@ inline static void ResetTaskInfo(void) {
   its_stack_size = 0;
   its_timeout = 0;
   its_delay_time = 0;
-}
-static void Reset(void) {
-  ResetTaskInfo();
   number_of_executions = 0;
   memset(return_codes, 0, sizeof(return_codes));
 }
@@ -50,60 +47,52 @@ static const UtkernelTskSpyMethodStruct kTheMethod = {
 };
 const UtkernelTskSpyMethod utkernelTskSpy = &kTheMethod;
 
-inline static void Enter(const char *system_call_name, INT id) {
-  systemCallLogger->Enter(system_call_name, id);
-}
-inline static INT Exit(const char *system_call_name) {
-  int return_code = return_codes[number_of_executions++];
-  systemCallLogger->Exit(system_call_name, return_code);
-  return return_code;
-}
-static INT Template(const char *system_call_name, INT id,
-                    void (*execute)(void)) {
-  Enter(system_call_name, id);
-  if (execute) execute();
-  return Exit(system_call_name);
-}
-inline static bool IsErrorCodeSet(void) {
-  return return_codes[number_of_executions] < 0;
-}
-static void _tk_cre_tsk(void) {
-  if (IsErrorCodeSet()) ResetTaskInfo();
-}
-ID tk_cre_tsk(CONST T_CTSK *pk_ctsk) {
+static void _tk_cre_tsk(const void *info) {
+  T_CTSK *pk_ctsk = (T_CTSK *)info;
   its_exinf = pk_ctsk->exinf;
   its_attribute = pk_ctsk->tskatr;
   its_task = pk_ctsk->task;
   its_priority = pk_ctsk->itskpri;
   its_stack_size = pk_ctsk->stksz;
-  return Template(__func__, kNonsenseParameter, _tk_cre_tsk);
 }
-ER tk_del_tsk(ID tskid) { return Template(__func__, tskid, NULL); }
-static void _tk_sta_tsk(void) {
-  if (!IsErrorCodeSet()) its_task(0, its_exinf);
+inline static INT TemplateWithoutId(const char *system_call_name,
+                                    ExecuteDelegate execute, const void *info) {
+  systemCallTemplate->SetReturnCode(return_codes[number_of_executions++]);
+  return systemCallTemplate->Execute(system_call_name, execute, info);
 }
+ID tk_cre_tsk(CONST T_CTSK *pk_ctsk) {
+  return TemplateWithoutId(__func__, _tk_cre_tsk, pk_ctsk);
+}
+inline static INT Template(const char *system_call_name, ID tskid,
+                           ExecuteDelegate execute, const void *info) {
+  systemCallTemplate->SetId(tskid);
+  systemCallTemplate->SetReturnCode(return_codes[number_of_executions++]);
+  return systemCallTemplate->Execute(system_call_name, execute, info);
+}
+ER tk_del_tsk(ID tskid) { return Template(__func__, tskid, NULL, NULL); }
+static void _tk_sta_tsk(const void *unused) { its_task(0, its_exinf); }
 ER tk_sta_tsk(ID tskid, INT stacd) {
-  return Template(__func__, tskid, _tk_sta_tsk);
+  return Template(__func__, tskid, _tk_sta_tsk, NULL);
 }
-void tk_ext_tsk(void) {
-  SetReturnCode(0, kNonsenseParameter);
-  Template(__func__, kNonsenseParameter, NULL);
+void tk_ext_tsk(void) { systemCallTemplate->Execute(__func__, NULL, NULL); }
+void tk_exd_tsk(void) { systemCallTemplate->Execute(__func__, NULL, NULL); }
+ER tk_ter_tsk(ID tskid) { return Template(__func__, tskid, NULL, NULL); }
+void _tk_slp_tsk(const void *info) {
+  TMO *tmout = (TMO *)info;
+  its_timeout = *tmout;
 }
-void tk_exd_tsk(void) {
-  SetReturnCode(0, kNonsenseParameter);
-  Template(__func__, kNonsenseParameter, NULL);
-}
-ER tk_ter_tsk(ID tskid) { return Template(__func__, tskid, NULL); }
 ER tk_slp_tsk(TMO tmout) {
-  its_timeout = tmout;
-  return Template(__func__, kNonsenseParameter, NULL);
+  return TemplateWithoutId(__func__, _tk_slp_tsk, &tmout);
 }
-ER tk_wup_tsk(ID tskid) { return Template(__func__, tskid, NULL); }
-ER tk_rel_wai(ID tskid) { return Template(__func__, tskid, NULL); }
-ER tk_sus_tsk(ID tskid) { return Template(__func__, tskid, NULL); }
-ER tk_rsm_tsk(ID tskid) { return Template(__func__, tskid, NULL); }
+ER tk_wup_tsk(ID tskid) { return Template(__func__, tskid, NULL, NULL); }
+ER tk_rel_wai(ID tskid) { return Template(__func__, tskid, NULL, NULL); }
+ER tk_sus_tsk(ID tskid) { return Template(__func__, tskid, NULL, NULL); }
+ER tk_rsm_tsk(ID tskid) { return Template(__func__, tskid, NULL, NULL); }
+void _tk_dly_tsk(const void *info) {
+  RELTIM *dlytim = (RELTIM *)info;
+  its_delay_time = *dlytim;
+}
 ER tk_dly_tsk(RELTIM dlytim) {
-  its_delay_time = dlytim;
-  return Template(__func__, kNonsenseParameter, NULL);
+  return TemplateWithoutId(__func__, _tk_dly_tsk, &dlytim);
 }
-ID tk_get_tid(void) { return Template(__func__, kNonsenseParameter, NULL); }
+ID tk_get_tid(void) { return TemplateWithoutId(__func__, NULL, NULL); }
