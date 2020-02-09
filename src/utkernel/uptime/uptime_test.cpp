d2123646c89d52ee48be5dabf13940dfb1bdde0d
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-02-09 22:49:54 +0900

    Add uptime module

---
+++ src/utkernel/uptime/uptime_test.cpp
@@ -0,0 +1,28 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "uptime.h"
+#include "utkernel/utkernel.h"
+}
+
+TEST(UptimeTest, Get) {
+  SYSTIM pk_tim = {0x01234567, 0x89ABCDEF};
+  tk_set_tim(&pk_tim);
+  EXPECT_EQ(0x0123456789ABCDEF, uptime->Get());
+
+  pk_tim.lo += 1;
+  tk_set_tim(&pk_tim);
+  EXPECT_EQ(0x0123456789ABCDF0, uptime->Get());
+
+  pk_tim.hi += 0x12345678;
+  pk_tim.lo += 0x90000000;
+  tk_set_tim(&pk_tim);
+  EXPECT_EQ(0x13579BE019ABCDF0, uptime->Get());
+
+  pk_tim.hi += 0x90000000;
+  pk_tim.lo += 0x00000001;
+  tk_set_tim(&pk_tim);
+  EXPECT_EQ(0xA3579BE019ABCDF1, uptime->Get());
+}
---
+++ v1/uptime.h
@@ -0,0 +1,15 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_UPTIME_H_
+#define V1_UPTIME_H_
+
+#include <stdint.h>
+
+typedef struct {
+  uint64_t (*Get)(void);  // The order of milliseconds
+} UptimeMethodStruct;
+typedef const UptimeMethodStruct* UptimeMethod;
+
+extern const UptimeMethod uptime;
+
+#endif  // V1_UPTIME_H_
