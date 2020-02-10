7a8fe27dcc5b24dc32c37a30f1c59ff1e315986e
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-02-10 17:06:09 +0900

    Use private method for new intervalTimer

--- src/utkernel/timer/interval_timer.c
+++ src/utkernel/timer/interval_timer.c
@@ -2,10 +2,6 @@
 // This software is released under the MIT License, see LICENSE.
 #include "interval_timer.h"
 
-#include <stdbool.h>
-#include <stddef.h>
-
-#include "bleu/v1/heap.h"
 #include "timer_private.h"
 #include "utkernel/utkernel.h"
 
@@ -14,29 +10,13 @@ static const TimerAbstractMethodStruct kConcreteMethod = {
     .Resume = Resume,
 };
 
-inline static bool Validate(TimerDelegate timer, int period_in_milliseconds) {
-  return timer && period_in_milliseconds > 0;
-}
 static void TimerEntry(void* exinf) {
   Timer self = (Timer)exinf;
   self->timer();
 }
-inline static bool CreateTimer(Timer self, int period_in_milliseconds) {
-  T_CCYC packet = {.exinf = self,
-                   .cycatr = TA_HLNG | TA_STA | TA_PHS,
-                   .cychdr = TimerEntry,
-                   .cyctim = period_in_milliseconds,
-                   .cycphs = period_in_milliseconds};
-  return (self->id = tk_cre_cyc(&packet)) >= 0;
-}
 static Timer New(TimerDelegate timer, int period_in_milliseconds) {
-  if (!Validate(timer, period_in_milliseconds)) return NULL;
-  Timer self = (Timer)heap->New(sizeof(TimerStruct));
-  if (!self) return self;
-  self->timer = timer;
-  self->impl = &kConcreteMethod;
-  if (!CreateTimer(self, period_in_milliseconds)) heap->Delete((void**)&self);
-  return self;
+  return _timer->New(timer, period_in_milliseconds, TimerEntry,
+                     &kConcreteMethod);
 }
 static const IntervalTimerMethodStruct kTheMethod = {
     .New = New,
--- src/utkernel/timer/timer_private.h
+++ src/utkernel/timer/timer_private.h
@@ -1,4 +1,4 @@
-// Copyright(c) 2020 Ken Okabe
+﻿// Copyright(c) 2020 Ken Okabe
 // This software is released under the MIT License, see LICENSE.
 #ifndef SRC_UTKERNEL_TIMER_TIMER_PRIVATE_H_
 #define SRC_UTKERNEL_TIMER_TIMER_PRIVATE_H_
@@ -10,5 +10,13 @@ typedef struct TimerStruct {
   TimerDelegate timer;
   TimerAbstractMethod impl;
 } TimerStruct;
+
+typedef struct {
+  Timer (*New)(TimerDelegate timer, int base_time_in_milliseconds,
+               void (*handler)(void* exinf), TimerAbstractMethod impl);
+} TimerPrivateMethodStruct;
+typedef const TimerPrivateMethodStruct* TimerPrivateMethod;
+
+extern const TimerPrivateMethod _timer;
 
 #endif  // SRC_UTKERNEL_TIMER_TIMER_PRIVATE_H_
