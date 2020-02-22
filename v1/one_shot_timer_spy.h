60704e6492a705e2965b3ba41a1f2af12ca4d4f1
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-02-22 19:40:12 +0900

    Add OneShotTimerSpy class

---
+++ v1/one_shot_timer_spy.h
@@ -0,0 +1,15 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_ONE_SHOT_TIMER_SPY_H_
+#define V1_ONE_SHOT_TIMER_SPY_H_
+
+typedef struct {
+  void (*Reset)(void);
+  int (*NewCalledCount)(void);
+  void (*Run)(int id);
+} OneShotTimerSpyMethodStruct;
+typedef const OneShotTimerSpyMethodStruct* OneShotTimerSpyMethod;
+
+extern const OneShotTimerSpyMethod oneShottimerSpy;
+
+#endif  // V1_ONE_SHOT_TIMER_SPY_H_
