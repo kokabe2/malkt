8812d6a5af91bc62d3f56743c1bd5c0aed0df02c
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-02-22 15:27:25 +0900

    Add TaskSpy

---
+++ src/utkernel/task/task_spy.c
@@ -0,0 +1,61 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "task_spy.h"
+
+#include "task.h"
+
+typedef struct TaskStruct {
+  int dummy;  //
+} TaskStruct;
+
+static TaskStruct task_dummy;
+static int new_called_count;
+static int delete_called_count;
+static int run_called_count;
+static int suspend_called_count;
+static int resume_called_count;
+static int delay_called_count;
+
+static void Reset(void) {
+  new_called_count = 0;
+  delete_called_count = 0;
+  run_called_count = 0;
+  suspend_called_count = 0;
+  resume_called_count = 0;
+  delay_called_count = 0;
+}
+static int NewCalledCount(void) { return new_called_count; }
+static int DeleteCalledCount(void) { return delete_called_count; }
+static int RunCalledCount(void) { return run_called_count; }
+static int SuspendCalledCount(void) { return suspend_called_count; }
+static int ResumeCalledCount(void) { return resume_called_count; }
+static int DelayCalledCount(void) { return delay_called_count; }
+static const TaskSpyMethodStruct kSpyMethod = {
+    .Reset = Reset,
+    .NewCalledCount = NewCalledCount,
+    .DeleteCalledCount = DeleteCalledCount,
+    .RunCalledCount = RunCalledCount,
+    .SuspendCalledCount = SuspendCalledCount,
+    .ResumeCalledCount = ResumeCalledCount,
+    .DelayCalledCount = DelayCalledCount,
+};
+const TaskSpyMethod task = &kSpyMethod;
+
+static Task New(ActionDelegate action, int priority, int stack_size) {
+  ++new_called_count;
+  return &task_dummy;
+}
+static void Delete(Task* self) { ++delete_called_count; }
+static void Run(Task self) { ++run_called_count; }
+static void Suspend(Task self) { ++suspend_called_count; }
+static void Resume(Task self) { ++resume_called_count; }
+static void Delay(int time_in_milliseconds) { ++delay_called_count; }
+static const TaskMethodStruct kTheMethod = {
+    .New = New,
+    .Delete = Delete,
+    .Run = Run,
+    .Suspend = Suspend,
+    .Resume = Resume,
+    .Delay = Delay,
+};
+const TaskMethod task = &kTheMethod;
